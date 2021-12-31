5ab974bc005a33bbf12131042acf566dc8afa67a
Raheel Junaid <raheelj2004@gmail.com>
2021-12-30 20:26:46 -0700

    Add real employees, add fixed hours, and fix time displaying error

--- main.py
+++ main.py
@@ -12,8 +12,9 @@ closing = 2100
 # special_hours = {}
 
 class Employee():
-    def __init__(self, name, availability):
+    def __init__(self, name, availability, fixed_hours=False):
         self.name = name
+        self.fixed_hours = fixed_hours
         self.availability = availability
         self.scheduled = []
 
@@ -59,37 +60,47 @@ closing_slots = [
     [closing - 800, closing + 30],
 ]
 
-bob = Employee("bob", [
-    [800, 2000],
-    [1200, 2400],
-    [800, 2000],
-    [1000, 2400],
+bandish = Employee("bandish", [
     None,
-    [1200, 2400],
+    [930, 1800],
+    [930, 1800],
+    [930, 1800],
+    [930, 1800],
+    [930, 1800],
+    None
+], True)
+
+therese = Employee("therese", [
+    [1030, 1830],
+    [1200, 2030],
+    [1200, 2030],
+    [1200, 2030],
+    [1300, 2130],
+    [1300, 2130],
+    [930, 2200]
+])
+
+jasmine = Employee("jasmine", [
+    None,
+    [1330, 2130],
+    [1330, 2130],
+    [1330, 2130],
+    [1330, 2130],
+    [1330, 2130],
     None,
 ])
 
-karen = Employee("karen", [
-    None,
-    [1200, 2400],
-    None,
-    [900, 1500],
-    [1200, 2400],
-    [1500, 2100],
-    [900, 2400],
+jamie = Employee("jamie", [
+    [0000, 2300],
+    [0000, 2300],
+    [0000, 2300],
+    [0000, 2300],
+    [0000, 2300],
+    [0000, 2300],
+    [0000, 2300],
 ])
 
-tyler = Employee("tyler", [
-    [1200, 2400],
-    [900, 1500],
-    None,
-    [800, 2000],
-    None,
-    [1000, 2400],
-    [1500, 2100],
-])
-
-employees = [bob, karen, tyler]
+employees = [jamie, jasmine, therese, bandish]
 randomized_employees = employees[:]
 
 for day_number, day in enumerate(days):
@@ -97,40 +108,66 @@ for day_number, day in enumerate(days):
     shuffle(randomized_employees)
 
     for employee in randomized_employees:
-        employee_availability = employee.availability[day_number]
-        if employee_availability != None:
-            potential_opening_slots = opening_slots[:]
-            for slot in opening_slots:
-                if slot[0] < employee_availability[0] or slot[1] > employee_availability[1]:
-                    potential_opening_slots.remove(slot)
-
-            potential_slots = regular_slots[:]
-            for slot in regular_slots:
-                if slot[0] < employee_availability[0] or slot[1] > employee_availability[1]:
-                    potential_slots.remove(slot)
-
-            potential_closing_slots = closing_slots[:]
-            for slot in closing_slots:
-                if slot[0] < employee_availability[0] or slot[1] > employee_availability[1]:
-                    potential_closing_slots.remove(slot)
-            
-            if len(day.emp_opening) < 2 and len(potential_opening_slots) != 0:
-                employee.scheduled.append(potential_opening_slots[randint(0, len(potential_opening_slots) - 1)])
+        if employee.fixed_hours:
+            employee_hours = employee.availability[day_number]
+            if employee.availability[day_number] in opening_slots:
+                employee.scheduled.append(employee.availability[day_number])
                 day.emp_opening.append(employee)
                 day.emp_working.append(employee)
 
-            elif len(day.emp_closing) < 2 and len(potential_closing_slots) != 0:
-                employee.scheduled.append(potential_closing_slots[randint(0, len(potential_closing_slots) - 1)])
+            elif employee.availability[day_number] in closing_slots:
+                employee.scheduled.append(employee.availability[day_number])
                 day.emp_closing.append(employee)
                 day.emp_working.append(employee)
 
-            else:
-                employee.scheduled.append(potential_slots[randint(0, len(potential_slots) - 1)])
+            elif employee.availability[day_number] in regular_slots:
+                employee.scheduled.append(employee.availability[day_number])
                 day.emp_working.append(employee)
 
-        else:
-            employee.scheduled.append(None)
+            else:
+                employee.scheduled.append(None)
 
+        else:
+            employee_availability = employee.availability[day_number]
+            if employee_availability != None:
+                potential_opening_slots = opening_slots[:]
+                for slot in opening_slots:
+                    if slot[0] < employee_availability[0] or slot[1] > employee_availability[1]:
+                        potential_opening_slots.remove(slot)
+
+                potential_slots = regular_slots[:]
+                for slot in regular_slots:
+                    if slot[0] < employee_availability[0] or slot[1] > employee_availability[1]:
+                        potential_slots.remove(slot)
+
+                potential_closing_slots = closing_slots[:]
+                for slot in closing_slots:
+                    if slot[0] < employee_availability[0] or slot[1] > employee_availability[1]:
+                        potential_closing_slots.remove(slot)
+                
+                if len(day.emp_opening) < 2 and len(potential_opening_slots) != 0:
+                    employee.scheduled.append(potential_opening_slots[randint(0, len(potential_opening_slots) - 1)])
+                    day.emp_opening.append(employee)
+                    day.emp_working.append(employee)
+
+                elif len(day.emp_closing) < 2 and len(potential_closing_slots) != 0:
+                    employee.scheduled.append(potential_closing_slots[randint(0, len(potential_closing_slots) - 1)])
+                    day.emp_closing.append(employee)
+                    day.emp_working.append(employee)
+
+                elif len(potential_slots) > 0:
+                    employee.scheduled.append(potential_slots[randint(0, len(potential_slots) - 1)])
+                    day.emp_working.append(employee)
+                
+                else:
+                    print(day.name, employee.name, employee.availability[day_number])
+                    employee.scheduled.append(employee.availability[day_number])
+                    day.emp_working.append(employee)
+
+            else:
+                employee.scheduled.append(None)
+
+employees.sort(key=lambda employee : employee.name)
 for employee in employees:
     schedule = []
     for day in employee.scheduled:
@@ -140,17 +177,23 @@ for employee in employees:
             start = day[0]
             end = day[1]
 
-            if start >= 1200:
-                start = start - 1200
+            if start >= 1300 and start < 2200:
+                start -= 1200
                 start = f"{str(start)[0:1]}:{str(start)[1:]}pm"
+
+            elif start >= 1200:
+                start = f"{str(start)[0:2]}:{str(start)[2:]}pm"
             elif start >= 1000:
                 start = f"{str(start)[0:2]}:{str(start)[2:]}am"
             else: 
                 start = f"{str(start)[0:1]}:{str(start)[1:]}am"
 
-            if end >= 1200:
-                end = end - 1200
+            if end >= 1300 and end < 2200:
+                end -= 1200
                 end = f"{str(end)[0:1]}:{str(end)[1:]}pm"
+
+            elif end >= 1200:
+                end = f"{str(end)[0:2]}:{str(end)[2:]}pm"
             elif end >= 1000:
                 end = f"{str(end)[0:2]}:{str(end)[2:]}am"
             else: 
