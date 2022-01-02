2eaea5d32fac66c59ddc30e41e2789b1626e1484
Raheel Junaid <raheelj2004@gmail.com>
2022-01-01 22:31:49 -0700

    Add variable opening and closing slots with dependent available slots

--- days.py
+++ days.py
@@ -1,16 +1,33 @@
 class Day():
-    def __init__(self, name):
+    def __init__(self, name, opening, closing):
         self.name = name
         self.emp_working = []
         self.emp_closing = []
         self.emp_opening = []
+        self.opening = opening
+        self.closing = closing
+        self.slots = [
+            [opening - 70, opening + 500],
+            [opening, opening + 500],
+            [closing - 500, closing],
+            [closing - 500, closing + 30],
+        ]
+        self.duration = self.closing - self.opening
+        
+        if self.duration >= 800:
+            self.slots.extend([
+                [opening - 70, opening + 800],
+                [opening, opening + 800],
+                [closing - 800, closing],
+                [closing - 800, closing + 30]
+            ])
 
-sunday = Day("sunday")
-monday = Day("monday")
-tuesday = Day("tuesday")
-wednesday = Day("wednesday")
-thursday = Day("thursday")
-friday = Day("friday")
-saturday = Day("saturday")
+sunday = Day("sunday", 1100, 1800)
+monday = Day("monday", 1100, 2000)
+tuesday = Day("tuesday", 1100, 2000)
+wednesday = Day("wednesday", 1100, 2000)
+thursday = Day("thursday", 1000, 2100)
+friday = Day("friday", 1000, 2100)
+saturday = Day("saturday", 1100, 1800)
 
 days = [sunday, monday, tuesday, wednesday, thursday, friday, saturday]
