8d1415b48c7b53fbb6f2af160715d775de4fa561
Raheel Junaid <raheelj2004@gmail.com>
2022-01-01 20:23:17 -0700

    Add employees, fix open/closing time slot quantity, 24 hr time

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from random import randint, shuffle
+from random import shuffle, sample
 from rich.table import Table
 from rich import print
 
@@ -45,21 +45,15 @@ days = [
     saturday
 ]
 
-opening_slots = [
+available_slots = [
     [opening - 70, opening + 500],
     [opening - 70, opening + 800],
-]
-
-regular_slots = [
     [opening, opening + 500],
     [opening, opening + 800],
     [closing - 500, closing],
     [closing - 800, closing],
-]
-
-closing_slots = [
     [closing - 500, closing + 30],
-    [closing - 800, closing + 30],
+    [closing - 800, closing + 30]
 ]
 
 bandish = Employee("bandish", [
@@ -122,77 +116,86 @@ ben_o = Employee("ben o", [
     [0000, 2300],
 ])
 
-def potential_slot(slots, employee_availability):
+ben_a = Employee("ben a", [
+    [1100, 1800],
+    None,
+    [1000, 1800],
+    [1200, 2000],
+    [1200, 2000],
+    None,
+    [1000, 1800],
+])
+
+monica = Employee("monica", [
+    [0000, 2300],
+    None,
+    None,
+    [1630, 2100],
+    None,
+    [1630, 2100],
+    None
+])
+
+def potential_slot(slots, employee_availability, day):
     potential_slots = slots[:]
     for slot in slots:
         if slot[0] < employee_availability[0] or slot[1] > employee_availability[1]:
             potential_slots.remove(slot)
+        elif slot[0] <= opening - 70 and len(day.emp_opening) >= emp_outside_of_hours:
+            potential_slots.remove(slot)
+        elif slot[1] >= closing + 30 and len(day.emp_closing) >= emp_outside_of_hours:
+            potential_slots.remove(slot)
     return potential_slots
 
 def book_employee(employee, slots, day):
-    if slots == []:
+    if slots == [] or slots is None:
         employee.scheduled.append(None)
     else:
         if type(slots[0]) is list:
-            slot = slots[randint(0, len(slots) - 1)]
+            if opening - 70 in map(lambda x: x[0], slots):
+                slots = [slot for slot in slots if slot[0] <= opening - 70]
+            elif closing + 30 in map(lambda x: x[1], slots):
+                slots = [slot for slot in slots if slot[1] >= closing + 30]
+            slot = sample(slots, 1)[0]
+
         else:
             slot = slots
 
-        if slot[0] == opening - 70 and len(day.emp_opening) < emp_outside_of_hours:
+        if slot[0] <= opening - 70:
             day.emp_opening.append(employee)
-        elif slot[1] == closing + 30 and len(day.emp_closing) < emp_outside_of_hours:
+        elif slot[1] >= closing + 30:
             day.emp_closing.append(employee)
 
         employee.scheduled.append(slot)
         day.emp_working.append(employee)
 
-employees = [jamie, jasmine, therese, bandish, aaron, ben_o]
-randomized_employees = employees[:]
+employees = [monica, ben_a, jamie, jasmine, therese, bandish, aaron, ben_o]
 
 for day_number, day in enumerate(days):
     table.add_column(day.name.capitalize())
-    shuffle(randomized_employees)
 
-    for employee in randomized_employees:
-        if employee.fixed_hours:
-            employee_hours = employee.availability[day_number]
-            if employee.availability[day_number] in opening_slots:
-                employee.scheduled.append(employee.availability[day_number])
-                day.emp_opening.append(employee)
-                day.emp_working.append(employee)
+    for employee in [employee for employee in employees if employee.fixed_hours]:
+        employee_hours = employee.availability[day_number]
+        book_employee(employee, employee_hours, day)
 
-            elif employee.availability[day_number] in closing_slots:
-                employee.scheduled.append(employee.availability[day_number])
-                day.emp_closing.append(employee)
-                day.emp_working.append(employee)
+    shuffle(employees)
+    for employee in [employee for employee in employees if not employee.fixed_hours]:
+        employee_availability = employee.availability[day_number]
+        if employee_availability != None:
+            potential_slots = potential_slot(available_slots, employee_availability, day)
 
-            elif employee.availability[day_number] in regular_slots:
-                employee.scheduled.append(employee.availability[day_number])
-                day.emp_working.append(employee)
-
+            if potential_slots == []:
+                book_employee(employee, employee_availability, day)
             else:
-                employee.scheduled.append(None)
+                book_employee(employee, potential_slots, day)
 
         else:
-            employee_availability = employee.availability[day_number]
-            if employee_availability != None:
-                potential_opening = potential_slot(opening_slots, employee_availability)
-                potential_closing = potential_slot(closing_slots, employee_availability)
-                potential_regular = potential_slot(regular_slots, employee_availability)
-
-                if not all([potential_opening, potential_closing, potential_regular]):
-                    book_employee(employee, employee_availability, day)
-                else:
-                    book_employee(employee, potential_opening, day)
-                    book_employee(employee, potential_closing, day)
-                    book_employee(employee, potential_regular, day)
-
-            else:
-                employee.scheduled.append(None)
+            employee.scheduled.append(None)
 
 employees.sort(key=lambda employee : employee.name)
 for employee in employees:
     schedule = []
+
     for day in employee.scheduled:
         if day == None:
             schedule.append("N/A")
@@ -200,27 +203,14 @@ for employee in employees:
             start = day[0]
             end = day[1]
 
-            if start >= 1300 and start < 2200:
-                start -= 1200
-                start = f"{str(start)[0:1]}:{str(start)[1:]}pm"
-
-            elif start >= 1200:
-                start = f"{str(start)[0:2]}:{str(start)[2:]}pm"
-            elif start >= 1000:
-                start = f"{str(start)[0:2]}:{str(start)[2:]}am"
-            else: 
-                start = f"{str(start)[0:1]}:{str(start)[1:]}am"
-
-            if end >= 1300 and end < 2200:
-                end -= 1200
-                end = f"{str(end)[0:1]}:{str(end)[1:]}pm"
-
-            elif end >= 1200:
-                end = f"{str(end)[0:2]}:{str(end)[2:]}pm"
-            elif end >= 1000:
-                end = f"{str(end)[0:2]}:{str(end)[2:]}am"
-            else: 
-                end = f"{str(end)[0:1]}:{str(end)[1:]}am"
+            if start < 1000:
+                start = f"{str(start)[0:1]}:{str(start)[1:]}"
+            else:
+                start = f"{str(start)[0:2]}:{str(start)[2:]}"
+            if end < 1000:
+                end = f"{str(end)[0:1]}:{str(end)[1:]}"
+            else:
+                end = f"{str(end)[0:2]}:{str(end)[2:]}"
 
             if day[0] <= opening - 30:
                 schedule.append(f"[green bold]{start}[/green bold] - {end}")
