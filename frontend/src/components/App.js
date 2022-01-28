76005b925b0eb062fe8ce88f68192d65970645fa
Raheel Junaid <raheelj2004@gmail.com>
2022-01-28 09:35:01 -0700

    Add Title

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,10 +1,15 @@
 // import styles from './App.module.scss'
 import Calendar from './Calendar.js'
+import { Typography } from 'antd'
+import 'antd/dist/antd.css'
+
+const { Title } = Typography
 
 function App() {
     return (
         <>
-            <h1>PASS</h1>
+            <Title>PASS</Title>
+            {/* TODO fix calendar next month on date click */}
             <Calendar />
         </>
     );
