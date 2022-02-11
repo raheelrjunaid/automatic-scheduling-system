ecb5e4d49f6380259470855fa535d53de45182b9
Raheel Junaid <raheelj2004@gmail.com>
2022-02-10 21:29:02 -0700

    add all CRUD ops on employee editing

--- frontend/src/components/Employees.js
+++ frontend/src/components/Employees.js
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react'
-import { Modal, Button, Spin, Select, Switch, Form, Input, Table, Tag, message } from 'antd'
-import { CloseOutlined, CheckOutlined } from '@ant-design/icons'
+import { Modal, Button, Spin, Select, Switch, Form, Input, Table, Tag, message, Space } from 'antd'
+import { PlusOutlined, CloseOutlined, CheckOutlined } from '@ant-design/icons'
 import axios from 'axios'
 
 export async function getAllEmployees(param=null) {
@@ -15,41 +15,71 @@ export async function getAllEmployees(param=null) {
 export default function Employees() {
     const [employees, setEmployees] = useState([])
     const [loading, setLoading] = useState(true)
+    const [formLoading, setFormLoading] = useState(false);
+    const [employeeEditing, setEmployeeEditing] = useState(null);
     const [modalVisibility, setModalVisibility] = useState(false);
     const [form] = Form.useForm();
 
+    async function setAllEmployees() {
+        setLoading(true)
+        const data = await getAllEmployees()
+        setEmployees(data.map((employee) => {
+            return {
+                key: employee._id,
+                first_name: employee.first_name,
+                last_name: employee.last_name,
+                fixed_hours: employee.fixed_hours,
+                colour: employee.colour
+            }
+        }))
+        setLoading(false)
+    }
+
     useEffect(() => {
         const controller = new AbortController()
-        async function setAllEmployees() {
-            const data = await getAllEmployees()
-            setEmployees(data.map((employee, indx) => {
-                return {
-                    key: indx,
-                    first_name: employee.first_name,
-                    last_name: employee.last_name,
-                    fixed_hours: employee.fixed_hours,
-                    colour: employee.colour
-                }
-            }))
-            setLoading(false)
-        }
         setAllEmployees()
         return () => controller.abort()
     }, [])
 
-    function toggleModal() {
+    function toggleModal(record) {
         setModalVisibility(!modalVisibility)
+        if (modalVisibility) {
+            setEmployeeEditing(null)
+        }
+        else if(record) {
+            setEmployeeEditing(record)
+            form.setFieldsValue(record)
+        }
+        else {
+            form.resetFields()
+            form.setFieldsValue({ colour: colours[0] })
+        }
     }
 
-    async function handleSubmit() {
+    async function deleteEmployee(record) {
         try {
-            const fields = await form.validateFields()
-            console.log(fields)
-            toggleModal()
+            await axios.delete(`/api/employees/${record.key}`)
+            setAllEmployees()
         } catch (error) {
             error.errorFields.forEach(({errors}) => {
                 message.error(errors)
             })
+        }
+    }
+
+    async function handleSubmit() {
+        try {
+            setFormLoading(true)
+            const data = await form.validateFields()
+            if (!data.fixed_hours) data.fixed_hours = false
+            if(employeeEditing) data._id = employeeEditing.key
+
+            employeeEditing ? await axios.put('/api/employees', data) : await axios.post('/api/employees', data)
+            setAllEmployees()
+            setFormLoading(false)
+            toggleModal()
+        } catch (error) {
+            console.error(error)
         }
     }
 
@@ -77,10 +107,17 @@ export default function Employees() {
             render: fixed_hours => fixed_hours ? <CheckOutlined /> : <CloseOutlined />
         },
         {
-            title: 'Edit',
-            dataIndex: 'edit',
-            key: 'edit',
-            render: _ => <Button type="default" onClick={toggleModal}>Edit</Button>
+            title: 'Operate',
+            dataIndex: 'operate',
+            key: 'operate',
+            render: (this_field, record) => (
+                <>
+                    <Space>
+                        <Button type="default" onClick={() => toggleModal(record)}>Edit</Button>
+                        <Button danger type="default" onClick={() => deleteEmployee(record)}>Delete</Button>
+                    </Space>
+                </>
+            )
         }
     ]
 
@@ -90,19 +127,20 @@ export default function Employees() {
         <>
             <Spin spinning={loading}>
                 <Table pagination={false} dataSource={employees} columns={columns} />
+                <Button icon={<PlusOutlined />} type="primary" block onClick={() => toggleModal()}>Add New Employee</Button>
             </Spin>
-            <Modal title="Edit employees" visible={modalVisibility} onOk={toggleModal} onCancel={toggleModal} footer={[
-                <Button type="default" onClick={toggleModal}>Cancel</Button>,
-                <Button type="primary" onClick={handleSubmit}>Submit</Button>,
+            <Modal title="Edit employees" visible={modalVisibility} onCancel={() => toggleModal()} footer={[
+                <Button type="default" onClick={() => toggleModal()}>Cancel</Button>,
+                <Button type="primary" loading={formLoading} onClick={handleSubmit}>Submit</Button>,
             ]}>
-                <Form form={form} initialValues={{ colour: colours[0] }}>
-                    <Form.Item name="first-name" label="First Name" rules={[{ required: true }]}>
+                <Form form={form}>
+                    <Form.Item name="first_name" label="First Name" rules={[{ required: true }]}>
                         <Input placeholder="First Name"/>
                     </Form.Item>
-                    <Form.Item name="last-name" label="Last Name" rules={[{ required: true }]}>
+                    <Form.Item name="last_name" label="Last Name" rules={[{ required: true }]}>
                         <Input placeholder="Last Name"/>
                     </Form.Item>
-                    <Form.Item name="fixed-hours" label="Fixed Hours">
+                    <Form.Item name="fixed_hours" label="Fixed Hours" valuePropName="checked">
                         <Switch />
                     </Form.Item>
                     <Form.Item name="colour" label="Colour">
