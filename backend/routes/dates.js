82c9c0cd45d241d6df4c29dba8ab5bb52893a8f8
Raheel Junaid <raheelj2004@gmail.com>
2022-02-22 11:43:22 -0700

    change date field to actual date field

--- backend/routes/dates.js
+++ backend/routes/dates.js
@@ -1,106 +1,100 @@
-import express from 'express'
-import { ObjectId } from 'mongodb'
-import { getDB } from '../db/conn.js'
+import express from "express";
+import { ObjectId } from "mongodb";
+import { getDB } from "../db/conn.js";
 
-const router = express.Router()
+const router = express.Router();
 
 router.get("/", (req, res) => {
-    const db = getDB()
-    db
-        .collection("dates")
-        .find()
-        .toArray((err, result) => {
-            if (err) {
-                console.log(err)
-                res.status(400).json({"message": "Error getting dates", err});
-            } else {
-                res.json({"message": "Successfully got dates", result})
-            }
-        })
-})
+  const db = getDB();
+  db.collection("dates")
+    .find()
+    .toArray((err, result) => {
+      if (err) {
+        console.log(err);
+        res.status(400).json({ message: "Error getting dates", err });
+      } else {
+        res.json({ message: "Successfully got dates", result });
+      }
+    });
+});
 
 // TODO: Add statement to check if date being modified exists (deletion or updating)
 
 router.get("/:date", (req, res) => {
-    const db = getDB()
-    db
-        .collection("dates")
-        .findOne({ date: new Date(req.params.date).toLocaleDateString() }, (err, result) => {
-            if (err) {
-                console.log(err)
-                res.status(400).json({"message": "Error getting date", err});
-            } else {
-                res.json({"message": "Successfully got date", result})
-            }
-        })
-})
+  const db = getDB();
+  db.collection("dates").findOne(
+    { date: new Date(req.params.date) },
+    (err, result) => {
+      if (err) {
+        console.log(err);
+        res.status(400).json({ message: "Error getting date", err });
+      } else {
+        res.json({ message: "Successfully got date", result });
+      }
+    }
+  );
+});
 
 router.post("/", (req, res) => {
-    const db = getDB()
-    const { date, opens, closes, min_emps_working } = req.body
+  const db = getDB();
+  const { date, opens, closes, min_emps_working } = req.body;
 
-    const newDate = {
-        date: new Date(date).toLocaleDateString(),
-        opens: opens,
-        closes: closes,
-        min_emps_working: min_emps_working
-    }
+  const newDate = {
+    date: new Date(date),
+    opens: opens,
+    closes: closes,
+    min_emps_working: min_emps_working,
+    availability: [],
+  };
 
-    // TODO make date unique to prevent duplicates
-    db
-        .collection("dates")
-        .insertOne(newDate, (err, result) => {
-            if (err) {
-                console.log(err)
-                res.status(400).json({"message": "Error creating date", err});
-            } else {
-                res.json({"message": "Successfully added date", result})
-            }
-        })
-})
-
-router.put("/", (req, res) => {
-    const db = getDB()
-    const body = req.body
-
-    let dateQuery
-    if (typeof(body._id) == "string") {
-        dateQuery = { _id: ObjectId(body._id)}
+  // TODO make date unique to prevent duplicates
+  db.collection("dates").insertOne(newDate, (err, result) => {
+    if (err) {
+      console.log(err);
+      res.status(400).json({ message: "Error creating date", err });
     } else {
-        dateQuery = { _id: body._id }
+      res.json({ message: "Successfully added date", result });
     }
+  });
+});
 
-    const { _id, ...updates } = req.body
+router.put("/:id", (req, res) => {
+  const db = getDB();
+  const body = req.body;
 
-    db
-        .collection("dates")
-        .updateOne(dateQuery, {
-            $set: updates
-        }, (err, result) => {
-            if (err) {
-                console.log(err)
-                res.status(400).json({"message": "Error updating date", "error": err});
-            } else {
-                res.json({"message": "Successfully updated date", result})
-            }
-        })
-})
+  const dateQuery = { _id: ObjectId(req.params.id) };
+
+  const { _id, ...updates } = req.body;
+
+  db.collection("dates").updateOne(
+    dateQuery,
+    {
+      $set: updates,
+    },
+    (err, result) => {
+      if (err) {
+        console.log(err);
+        res.status(400).json({ message: "Error updating date", error: err });
+      } else {
+        res.json({ message: "Successfully updated date", result });
+      }
+    }
+  );
+});
 
 router.delete("/:id", (req, res) => {
-    const db = getDB()
+  const db = getDB();
 
-    let dateQuery = { _id: ObjectId(req.params.id)}
+  let dateQuery = { _id: ObjectId(req.params.id) };
 
-    db
-        .collection("dates")
-        .deleteOne(dateQuery, (err, result) => {
-            if (err) {
-                console.log(err)
-                res.status(400).json({"message": "Error deleting date", err});
-            } else {
-                res.json({"message": "Successfully deleted date", result})
-            }
-        })
-})
+  db.collection("dates").deleteOne(dateQuery, (err, result) => {
+    if (err) {
+      console.log(err);
+      res.status(400).json({ message: "Error deleting date", err });
+    } else {
+      res.json({ message: "Successfully deleted date", result });
+    }
+  });
+});
 
-export default router
+export default router;
